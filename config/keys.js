dcf6ee4ffa3be2aa5e334caa8708b679aaaed1e8
khanhduong1409 <khanhduong140900@gmail.com>
2023-09-20 09:40:58 +0700

    todov3

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,17 @@
+// exports.mysql = {
+//   host: process.env.DBHOST,
+//   port: 3306,
+//   database: process.env.DBNAME,
+//   user: process.env.DBUSER,
+//   password: process.env.DBPASSWORD,
+//   socketPath: process.env.INSTANCE_UNIX_SOCKET
+// };
+
 exports.mysql = {
-  host: process.env.DBHOST,
+  host: 10.186.160.3,
   port: 3306,
-  database: process.env.DBNAME,
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
+  database: database,
+  user: duong,
+  password: 123456$,
   socketPath: process.env.INSTANCE_UNIX_SOCKET
 };
